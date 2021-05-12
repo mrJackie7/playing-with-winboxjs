f17bcf8bc71585fd85324d716c00083a9160846a
joshuachristianto <joshuachritianto7@gmail.com>
2021-05-12 22:50:59 +0700

    V.1.0.0: First commit initiated!

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Portfolio (Windows Command Promt in blue color)
+
+### 12 Mei 2021
+
+## About This Project
+
+Jadi pada dasarnya ini adalah portfolio versi ketiga pada kali ini bertemakan terminal, terinspirasi dari [video ini](https://www.youtube.com/watch?v=jQCk2yo10YY&ab_channel=TraversyMedia) dengan bantuan [javascript bundler ini](https://nextapps-de.github.io/winbox/)
+
+## Version
+
+- 1.0.0: First Commit initiated!
---
+++ js/main.js
@@ -0,0 +1,42 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    width: '500px',
+    height: '500px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#209cee')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    width: '500px',
+    height: '500px',
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#209cee')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
